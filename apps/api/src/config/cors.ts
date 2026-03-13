0cd7e5fd59c5c4ecf45262da455f779cb9effc04
Mohamed Aziz Ben Hlel <tigana137@gmail.com>
2026-03-13 18:12:24 +0100

    fixes for prod

--- apps/api/src/config/cors.ts
+++ apps/api/src/config/cors.ts
@@ -5,7 +5,7 @@ export function configureCors() {
   return cors({
     // origin: new RegExp(ENV.ALLOWED_ORIGIN_PATTERNS || '.*'),
     origin: '*',
-
+    // !
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
     exposedHeaders: ['Content-Length'],
